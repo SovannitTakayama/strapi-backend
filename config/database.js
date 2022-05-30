6c4a9fe74fc553ca9b81aca447814d761d00329e
sovannit <sovannit.o@codium.co>
2022-05-30 15:52:24 +0700

    Update database config

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-54-172-175-251.compute-1.amazonaws.com'),
+      host: env('DATABASE_HOST', 'ec2-54-204-56-171.compute-1.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'ddaehuiqu163u9'),
-      user: env('DATABASE_USERNAME', 'fysaorrgvlhkwl'),
-      password: env('DATABASE_PASSWORD', 'a100e7d741fa1ef5fe4c0073d71009c65715931a71e24be3a378041952add7a5'),
+      database: env('DATABASE_NAME', 'd8ocuvok8de8tj'),
+      user: env('DATABASE_USERNAME', 'wqjbsoutrisqao'),
+      password: env('DATABASE_PASSWORD', '2504ae836190d18d1b19c2d1d585a33bb021b282454482b091e01e0533db27f8'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
